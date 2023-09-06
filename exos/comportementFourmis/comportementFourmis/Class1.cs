fc37c6594ce6ace9b40b2d8784421a532da0b02d
termimi <sofiene1023@gmail.com>
2023-09-06 08:38:02 +0200

    Rattrapage commmit
    Rattrapage des commit exos snail + parachute

---
+++ exos/comportementFourmis/comportementFourmis/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomStrategy;
+
+namespace comportementFourmis
+{
+    public class XXXBlueQueen : QueenStrategy
+    {
+        public override StrategyColor GetColor()
+        {
+            return StrategyColor.Blue;
+        }
+        public override string GetId()
+        {
+            return "demo-blue-queen";
+        }
+        public override int GetVersion()
+        {
+            return 1;
+        }
+        public override string GetMessage()
+        {
+            return "Hello Demo";
+        }
+    }
+
+}
